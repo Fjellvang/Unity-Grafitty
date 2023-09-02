20b889eeeea8ac8b8a543f94603175ded120fcf7
AndreasKadk <s220383@edu.kglakademi.dk>
2023-09-02 16:32:25 +0200

    JumpMF

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    private Vector2 move;
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        move = context.ReadValue<Vector2>();
-    }
+    #region Variables: Movement
+
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+
+    [SerializeField] private float speed;
+
+    #endregion
+    #region Variables: Rotation
+
+    [SerializeField] private float smoothTime = 0.05f;
+    private float _currentVelocity;
+
+    #endregion
+    #region Variables: Gravity
+
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float _velocity;
+
+    #endregion
+    #region Variables: Jumping
+
+    [SerializeField] private float jumpPower;
+
+    #endregion
     
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        MovePlayer();
+        _characterController = GetComponent<CharacterController>();
     }
 
-    public void MovePlayer()
+    private void Update()
     {
-        Vector3 movement = new Vector3(move.x,0f, move.y);
-        if  (movement!= Vector3.zero) {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    private void ApplyGravity()
+    {
+        if (IsGrounded() && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
         }
         
-
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        _direction.y = _velocity;
     }
+    
+    private void ApplyRotation()
+    {
+        if (_input.sqrMagnitude == 0) return;
+        
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        if (!IsGrounded()) return;
+
+        _velocity += jumpPower;
+    }
+
+    private bool IsGrounded() => _characterController.isGrounded;
 }

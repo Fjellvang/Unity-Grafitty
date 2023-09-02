d9269301393059b4d6e01f65e93386df8870cddb
aleksander fjellvang <asf@appstract.dk>
2023-09-02 16:05:29 +0200

    issue

--- Assets/Scripts/Guard/GuardPatrolState.cs
+++ Assets/Scripts/Guard/GuardPatrolState.cs
@@ -4,7 +4,7 @@ public class GuardPatrolState : GuardState
 {
     public override void OnEnter()
     {
-        var point = Controller.WaypointCollection.Points[Random.Range(0, Controller.WaypointCollection.Points.Length)];
+        var point = Controller.WaypointCollection.Points[Random.Range(0, Controller.WaypointCollection.Points.Length-1)];
         Controller.Movement.SetTarget(point);
 
         Controller.Movement.OnTargetReached += Movement_OnTargetReached;
